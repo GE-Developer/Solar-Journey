6a7075e0d61d0e3b38dce578e8e166c9cb990701
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-04-16 01:00:02 +0300

    Initial Commit

---
+++ Solar Journey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solar Journey
+//
+//  Created by Mikhail Bukhrashvili on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
