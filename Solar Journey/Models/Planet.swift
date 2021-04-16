ac68106af2c8e4ae9d8f6e99460961ee74c4403f
BulatKamalov1985 <Kamalovmarket@gmail.com>
2021-04-16 09:55:48 +0300

    three

---
+++ Solar Journey/Models/Planet.swift
@@ -0,0 +1,16 @@
+//
+//  Planets.swift
+//  Solar Journey
+//
+//  Created by Bulat Kamalov on 16.04.2021.
+//
+struct Planet {
+    
+    let orbitRadius: Int
+    let daysInYear: Int
+    let raduis: Double
+    let mass: Double
+    let hoursInDay: Double
+    let g: Double
+    let temperature: Int
+}
